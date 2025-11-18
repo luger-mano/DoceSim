4e2fba542456e930047ed2da2201d5243c37ac3c
luger-mano <lucas.germanysantos@gmail.com>
2025-11-18 16:31:30 -0300

    Adicionei funções para criação de produtos

--- BackEnd/controller/product_controller.go
+++ BackEnd/controller/product_controller.go
@@ -8,14 +8,75 @@ import (
 )
 
 func ProductsCreate(ctx *gin.Context) {
-	product := model.Product{Name: "Brigadeiro", Description: "Brigadeiro com confetes", Value: 9.5}
 
+	var body struct {
+		Name        string
+		Description string
+		Value       float64
+	}
+
+	ctx.Bind(&body)
+
+	product := model.Product{Name: body.Name, Description: body.Description, Value: body.Value}
 	response := database.DB.Create(&product)
 
 	if response.Error != nil {
 		ctx.Status(400)
 		return
 	}
+	ctx.JSON(200, gin.H{
+		"product": product,
+	})
+}
+
+func ProductsShow(ctx *gin.Context) {
+	var products []model.Product
+	database.DB.Find(&products)
+
+	ctx.JSON(200, gin.H{
+		"products": products,
+	})
+}
+
+func ProductsIndex(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var product model.Product
+	database.DB.First(&product, id)
+
+	ctx.JSON(200, gin.H{
+		"product": product,
+	})
+}
+
+func ProductsUpdate(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var body struct {
+		Name        string
+		Description string
+		Value       float64
+	}
+
+	ctx.Bind(&body)
+
+	var product model.Product
+	database.DB.First(&product, id)
+
+	database.DB.Model(&product).Updates(model.Product{Name: body.Name, Description: body.Description, Value: body.Value})
+
+	ctx.JSON(200, gin.H{
+		"product": product,
+	})
+}
+func ProductsDelete(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var product model.Product
+	database.DB.First(&product, id)
+
+	database.DB.Delete(&product)
+
 	ctx.JSON(200, gin.H{
 		"product": product,
 	})
