146c5247075a137e28869df36446e1d5dd5848de
luger-mano <lucas.germanysantos@gmail.com>
2025-11-18 16:31:10 -0300

    Adicionei todos os métodos da API

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -18,6 +18,10 @@ func main() {
 	DB.AutoMigrate(&model.Product{})
 
 	server.POST("/products", controller.ProductsCreate)
+	server.PUT("/products/:id", controller.ProductsUpdate)
+	server.DELETE("/products/:id", controller.ProductsDelete)
+	server.GET("/products", controller.ProductsShow)
+	server.GET("/products/:id", controller.ProductsIndex)
 
 	server.Run(":8000")
 }
